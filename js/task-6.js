fd7670ae964fc3870dff6f1be8852c20bbd38183
redreren <jelka0503@gmail.com>
2020-09-04 18:37:35 +0300

    task6 done

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,12 @@
 "use strict";
+let input = confirm('Хотите числа вводить?');
+let total = 0;
+
+total = ++total;
+
+while (input) {
+    input = +prompt();
+    total += input;
+    console.log(input);
+}
+console.log(total);
